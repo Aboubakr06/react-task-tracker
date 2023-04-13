f51c822bdde9ed901f5f4fe5ff8e47dbb07e707a
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-04-13 13:21:46 +0000

    add header and btn

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+// import React from 'react' (for class use) 
+import Header from './Header'
+
+function App() {
+  return (
+    <div className='container'>
+    <Header />   
+    </div>
+  )
+}
+
+export default App
